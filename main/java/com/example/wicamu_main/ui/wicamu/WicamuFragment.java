97a70e5e361fb27379c3a6a26352c2ee8b8710c3
haru <haruhi.12.yamaguchi.22@gmail.com>
2020-01-27 18:22:14 +0900

    図形を表示成功

--- main/java/com/example/wicamu_main/ui/wicamu/WicamuFragment.java
+++ main/java/com/example/wicamu_main/ui/wicamu/WicamuFragment.java
@@ -9,12 +9,44 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.os.Bundle;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
 import com.example.wicamu_main.R;
 
+
+public class WicamuFragment extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_wicamu, container, false);
+        final TextView textView = root.findViewById(R.id.text_wicamu);
+
+        class main_wicamu extends AppCompatActivity {
+
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.fragment_wicamu);
+
+                textView.setText("s");
+            }
+        }
+
+
+        return root;
+    }
+}
+
+
+/*
 
 public class WicamuFragment extends Fragment {
     private WicamuViewModel wicamuViewModel;
@@ -35,4 +67,5 @@ public class WicamuFragment extends Fragment {
     }
 }
 
+ */
 
--- main/java/com/example/wicamu_main/ui/wicamu/WicamuViewModel.java
+++ main/java/com/example/wicamu_main/ui/wicamu/WicamuViewModel.java
@@ -1,19 +1,45 @@
 package com.example.wicamu_main.ui.wicamu;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
 
-public class WicamuViewModel extends ViewModel{
+public class WicamuViewModel extends View {
 
-    private MutableLiveData<String> mText;
+    Paint paint;
 
-    public WicamuViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("aaaaaaaaaaaaaaaat");
+    public WicamuViewModel(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        paint = new Paint();
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // 背景、半透明
+        canvas.drawColor(Color.argb(127, 0, 127, 63));
+
+        // 円
+        paint.setColor(Color.argb(255, 68, 255, 255));
+        paint.setStrokeWidth(30);
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.STROKE);
+        // (x1,y1,r,paint) 中心x1座標, 中心y1座標, r半径
+        canvas.drawCircle(450, 450, 100, paint);
+
+        // 矩形
+        paint.setColor(Color.argb(255, 255, 190, 0));
+        paint.setStrokeWidth(10);
+        paint.setStyle(Paint.Style.STROKE);
+        // (x1,y1,x2,y2,paint) 左上の座標(x1,y1), 右下の座標(x2,y2)
+        canvas.drawRect(480, 480, 850, 880, paint);
+
+        // 線
+        paint.setStrokeWidth(15);
+        paint.setColor(Color.argb(255, 0, 255, 120));
+        // (x1,y1,x2,y2,paint) 始点の座標(x1,y1), 終点の座標(x2,y2)
+        canvas.drawLine(350, 850, 750, 630, paint);
     }
 }
